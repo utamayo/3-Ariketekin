8ba0e374a9aaaad3792e685358571e8470e8889f
Urko <43012012+utamayo@users.noreply.github.com>
2022-11-10 11:58:55 +0100

    Create 2.Ariketa.c

---
+++ 2.Ariketa.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+  char izena[10];
+  printf("Sartu zure izena: ");
+  scanf("%s",izena); //izena aldagaian erabiltzaileak sartutakoa gordeko du.
+  printf("Kaixo %s",izena);
+}
